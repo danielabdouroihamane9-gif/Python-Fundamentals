d58eee56d47c763373e2ec26d6d51c7e2a2afe90
danielabdouroihamane9-gif <danielabdouroihamane9@gmail.com>
2026-05-13 02:19:13 +0800

    Update salary tracker with new features on class attribute, which store base salaries for each level.
    Validate name and level are string type and validate level to be a value of the dictionary.
    
    Initialize salary getter methodto accessthe private attribute _salary.

--- 33_Advanced-OOP/3_salary_tracker.py
+++ 33_Advanced-OOP/3_salary_tracker.py
@@ -1,18 +1,44 @@
 class Employee:
-    def __init__(self, name, level):
-        self._name = name
-        self._level = level
+    # class attribute to store base salaries for each level
+    _base_salaries = {
+        'trainee': 1000,
+        'junior': 2000,
+        'mid-level': 3000,
+        'senior': 4000,
+    }
 
-    def __str__(self):
-        return f'{self._name}: {self._level}'
+    def __init__(self, name, level):
+        # Validate that 'name' and 'level' are of type 'str' 
+        if not (isinstance(name, str) and isinstance(level, str)):
+            raise TypeError("'name' and 'level' attribute must be of type 'str'.")
+        # Validate that 'level' is one of the allowed values in the _base_salaries dictionary
+        if level not in Employee._base_salaries:
+            raise ValueError(f"Invalid value '{level}' for 'level' attribute.")
+        
+        self._name = name       
+        self._level = level
+        self._salary = Employee._base_salaries[level]   # Set the salary based on the level using the class attribute _base_salaries
+
+    def __str__(self):      # String representation of the Employee object, showing the name and level.
+        return f'{self.name}: {self.level}'
+
+    def __repr__(self):     # Official string representation of the Employee object, showing the name and level in a format that can be used to recreate the object.
+        return f"Employee('{self.name}', '{self.level}')"
 
     @property
-    def name(self):
+    def name(self):     # Getter method for the 'name' attribute, allowing access to the private attribute _name.
         return self._name
 
+
     @property
-    def level(self):
+    def level(self):    # Getter method for the 'level' attribute, allowing access to the private attribute _level.
         return self._level
+
+    @property
+    def salary(self):   # Getter method for the 'salary' attribute, allowing access to the private attribute _salary.
+        return self._salary
+
+
 charlie_brown = Employee('Charlie Brown', 'trainee')
 print(charlie_brown)
-print(charlie_brown.name)
+print(f'Base salary: ${charlie_brown.salary}')  # Output: Base salary: $1000
